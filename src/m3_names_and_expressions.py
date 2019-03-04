efdcde706b0eac85c8dc35b1dfd788bc79ca2e5a
Yang Chenxu <yangc2@rose-hulman.edu>
2019-03-04 16:44:47 -0500

    Done

--- src/m3_names_and_expressions.py
+++ src/m3_names_and_expressions.py
@@ -1,9 +1,11 @@
+y = 2 ** 5
+print(y * 100)
 
-answer = 2 ** 5
-print(answer * 100)
+
+
 
 ###############################################################################
-# TODO: 1.
+# Done: 1.
 #   Read the 2 lines of code ABOVE this _TODO_.  That code:
 #     1. Computes 2 raised to the 5th power,
 #          yielding the object that is the integer 32.
@@ -18,9 +20,14 @@ print(answer * 100)
 #   Once you completely understand the above, run this module,
 #   confirming that it prints 3200.  Then change the above _TODO_ to DONE.
 ###############################################################################
+import math
+hi= math.cos(2.75)
+answer = 77+ hi
+print(math.sqrt(answer))
+
 
 ###############################################################################
-# TODO: 2.
+# Done: 2.
 #   Immediately below this _TODO_, write code that:
 #     - Computes 77 plus the cosine of 2.75.
 #         HINT: You will need to import the   math  module (library).
@@ -28,17 +35,27 @@ print(answer * 100)
 #     - Prints the square root of that computed value.
 #   Run your code (fix errors as needed), then change the above _TODO_ to DONE.
 ###############################################################################
+import math
+y=41*88
+x=4* math.cos(2)
+t=math.sqrt(x+y)
+print(t)
 
 ###############################################################################
-# TODO: 3.
+# Done: 3.
 #   Immediately below this _TODO_, write code that computes and prints:
 #      the square root of ((41 * 88) + (4 * the cosine of 2))
 #   Use as few or as many intermediate names as you feel appropriate.
 #   Run your code (fix errors as needed), then change the above _TODO_ to DONE.
 ###############################################################################
+import math
+y= math.sqrt(2)
+print(y)
+x=(2**0.5)
+print(x)
 
 ###############################################################################
-# TODO: 4.
+# Done: 4.
 #   Immediately below this _TODO_,
 #   write code that computes the square root of 2 in two ways:
 #     - By using the   math.sqrt   function.
